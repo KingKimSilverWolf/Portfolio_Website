f4797ee8b2ec71debacfef0638dbed8340c63a84
KingKimSilverWolf <129137737+KingKimSilverWolf@users.noreply.github.com>
2024-03-15 01:30:41 -0400

    Social Icons

---
+++ my-react-app/src/components/home/Data.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Data = () => {
+  return (
+    <div className="home__data">
+        <h1 className="home__title">
+            Kim Magidhi
+            <svg
+                    width="36"
+                    height="36"
+                    viewBox="0 0 48 48"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="home__hand"
+                >
+                    <path
+                        d="M25.4995 32.0305L31.3495 33.1555L36.1495 8.48051C36.4495 6.83051 35.3995 5.18051
+                        33.82454.88051C32.1745 4.58051 30.5995 5.70551 30.2995 7.35551L25.4995 32.0305Z"
+                        fill='#FFDD67'
+                    ></path>
+                    <path
+                        d="M33.8996 4.88018C33.6746 4.80518 33.5246 4.80518 33.2996 4.80518C34.6496 5.33018
+                        35.3996 6.75518 35.0996 8.25518L30.2996 32.9302L31.3496 33.1552L36.1496 8.48018C36.5246
+                        6.75518 35.4746 5.18018 33.8996 4.88018Z"
+                        fill="#EBA352"
+                    ></path>
+    
+            </svg>
+        </h1>
+    </div>
+  )
+}
+
+export default Data
