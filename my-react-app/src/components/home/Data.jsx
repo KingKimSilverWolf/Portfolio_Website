193abbfa2d9581b6f2ae41edd1699101470ddab6
KingKimSilverWolf <129137737+KingKimSilverWolf@users.noreply.github.com>
2024-03-16 13:32:45 -0400

    home page edit

--- my-react-app/src/components/home/Data.jsx
+++ my-react-app/src/components/home/Data.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { ReactComponent as WavingHandEmoji } from '../../assets/hand1.svg'
-
-
 
 const Data = () => {
   return (
     <div className="home__data">
         <h1 className="home__title">
-            Kim Magidhi              
-            <WavingHandEmoji />
+            Kim Magidhi             
         </h1>
         <h3 className="home__subtitle"> Software Developer </h3>
         <p className="home__description">I'm an aspiring Software Developer aiming to excel 
