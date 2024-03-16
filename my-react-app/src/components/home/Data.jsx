442817fdfe849e527da00b15a045748d54ebd865
KingKimSilverWolf <129137737+KingKimSilverWolf@users.noreply.github.com>
2024-03-16 12:41:36 -0400

    Update Data.jsx

--- my-react-app/src/components/home/Data.jsx
+++ my-react-app/src/components/home/Data.jsx
@@ -15,7 +15,9 @@ const Data = () => {
         as a Full Stack Developer. I'm committed to mastering both frontend and backend skills 
         to create impactful software solutions efficiently.</p>
 
-        <a href="#contact" className="button button--flex">Say HelloHello🙋🏽‍♂️</a>
+        <a href="#contact" className="button">
+            Say Hello 🙋🏽‍♂️
+        </a>
     </div>
   )
 }
