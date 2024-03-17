497ec491d58c6f8c5ee8bc572f414c4acb0784eb
KingKimSilverWolf <129137737+KingKimSilverWolf@users.noreply.github.com>
2024-03-16 20:19:05 -0400

    home page edits

--- my-react-app/src/components/home/Data.jsx
+++ my-react-app/src/components/home/Data.jsx
@@ -4,14 +4,14 @@ const Data = () => {
   return (
     <div className="home__data">
         <h1 className="home__title">
-            Kim Magidhi             
+            Kim Magidhi         
         </h1>
-        <h3 className="home__subtitle"> Software Developer </h3>
+        <h3 className="home__subtitle"> Software Developer 👨🏾‍💻</h3>
         <p className="home__description">I'm an aspiring Software Developer aiming to excel 
         as a Full Stack Developer. I'm committed to mastering both frontend and backend skills 
         to create impactful software solutions efficiently.</p>
 
-        <a href="#contact" className="button">
+        <a href="#contact" className="button button--flex">
             Say Hello 🙋🏽‍♂️
         </a>
     </div>
