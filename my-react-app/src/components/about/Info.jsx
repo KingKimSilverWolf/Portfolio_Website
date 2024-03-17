d5ecab2b60ce3afcbd15154d558802fbf0a43024
KingKimSilverWolf <129137737+KingKimSilverWolf@users.noreply.github.com>
2024-03-17 12:37:30 -0400

    Info data

--- my-react-app/src/components/about/Info.jsx
+++ my-react-app/src/components/about/Info.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class='bx bx-award'></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">4+ Years of programming</span>
+      </div>
+
+      <div className="about__box">
+        <i className="bx bx-briefcase-alt"></i>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">Associates Degree in Science: <b>Computer Science</b></span>
+      </div>
+
+      <div className="about__box">
+        <i class='bx bxl-java'></i><i class='bx bxl-python'></i>
+        <h3 className="about__title">Favourite Languages</h3>
+        <span className="about__subtitle">Java & Python</span>
+      </div>
+    </div>
   )
 }
 
