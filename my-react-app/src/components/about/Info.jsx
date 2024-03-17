ac39761a25620bc84d2bdbfc4143da231645f0bd
KingKimSilverWolf <129137737+KingKimSilverWolf@users.noreply.github.com>
2024-03-17 14:46:40 -0400

    about section edits

--- my-react-app/src/components/about/Info.jsx
+++ my-react-app/src/components/about/Info.jsx
@@ -4,21 +4,24 @@ const Info = () => {
   return (
     <div className="about__info grid">
       <div className="about__box">
-        <i class='bx bx-award'></i>
+        <i class='bx bx-award about__icon'></i>
         <h3 className="about__title">Experience</h3>
         <span className="about__subtitle">4+ Years of programming</span>
       </div>
 
       <div className="about__box">
-        <i className="bx bx-briefcase-alt"></i>
+        <i className="bx bx-briefcase-alt about__icon"></i>
         <h3 className="about__title">Completed</h3>
-        <span className="about__subtitle">Associates Degree in Science: <b>Computer Science</b></span>
+        <span className="about__subtitle">Asc. in <b>Computer Science</b></span>
       </div>
 
       <div className="about__box">
-        <i class='bx bxl-java'></i><i class='bx bxl-python'></i>
-        <h3 className="about__title">Favourite Languages</h3>
-        <span className="about__subtitle">Java & Python</span>
+        <i class='bx bxl-java about__icon'></i><i class='bx bxl-python about__icon'></i><i class='bx bxs-file-json about__icon'></i>
+        <i class='bx bxl-react about__icon'></i>
+        <i class='bx bxl-typescript about__icon'></i><i class='bx bxl-c-plus-plus about__icon'></i><i class='bx bxl-flutter about__icon'></i><i class='bx bxl-tailwind-css about__icon'></i>
+        <i class='bx bxl-javascript about__icon'></i><i class='bx bxl-git about__icon'></i><i class='bx bxl-github about__icon'></i><i class='bx bxl-nodejs about__icon'></i>
+        <h3 className="about__title">Familia Languages & Interfaces</h3>
+        <span className="about__subtitle"></span>
       </div>
     </div>
   )
